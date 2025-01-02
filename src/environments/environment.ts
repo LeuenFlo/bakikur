be15fc8818d713d0f165fc2665baf39b698e1f3c
Florian Leuenberger <leuenberger.florian@gmx.ch>
2025-01-02 16:46:41 +0100

    chore: Update environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   /*
-  apiUrl: 'https://bakikur-backend.onrender.com/api',
-  baseUrl: 'https://bakikur-backend.onrender.com'
-  */
   apiUrl: 'http://localhost:5040/api',
   baseUrl: 'http://localhost:5040',
+  */
+  apiUrl: 'https://bakikur-backend.onrender.com/api',
+  baseUrl: 'https://bakikur-backend.onrender.com'
 };
